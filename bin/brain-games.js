19b444c2061133bbaff123aa9b11432bab193317
Boris P <pustovetov.boris@yandex.ru>
2020-04-15 17:35:37 +0300

    fix code

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line no-unused-vars
 import userName from '../src/index.js';
-
-const Greeting = userName;
-console.log(Greeting);
 
 console.log('Welcome to the Brain Games!');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const userName = readlineSync.question('May I have your name? ');
-return `Hello,${userName}!`;
+console.log(`Hello,${userName}!`);
 
 // eslint-disable-next-line no-unreachable
 export default userName;
